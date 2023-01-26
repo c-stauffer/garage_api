93bc73653b11335e6bdd16cdebb0df938815eb2f
chris Stauffer <cstauffer@outlook.com>
2023-01-26 17:52:46 -0500

    ready to test

--- cmd/main.go
+++ cmd/main.go
@@ -9,14 +9,41 @@ import (
 	"github.com/rs/cors"
 )
 
+// func BasicAuth(handler http.HandlerFunc, username, password, realm string) http.HandlerFunc {
+
+// 	return func(w http.ResponseWriter, r *http.Request) {
+
+// 		user, pass, ok := r.BasicAuth()
+
+// 		if !ok || subtle.ConstantTimeCompare([]byte(user), []byte(username)) != 1 || subtle.ConstantTimeCompare([]byte(pass), []byte(password)) != 1 {
+// 			w.Header().Set("WWW-Authenticate", `Basic realm="`+realm+`"`)
+// 			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+// 			return
+// 		}
+
+// 		handler(w, r)
+// 	}
+// }
+
 func main() {
 	router := mux.NewRouter()
+	// user := "username"
+	// pass := "password"
+	// realm := "Enter username and password."
 
 	router.HandleFunc("/actuate", handlers.Actuate).Methods(http.MethodGet)
-	router.HandleFunc("/state", handlers.GetState).Methods(http.MethodGet)
+	router.HandleFunc("/ping", handlers.Actuate).Methods(http.MethodGet)
+	router.HandleFunc("/doorstate", handlers.GetState).Methods(http.MethodGet)
 	router.HandleFunc("/temperature", handlers.GetTemperature).Methods(http.MethodGet)
 	router.HandleFunc("/humidity", handlers.GetHumidity).Methods(http.MethodGet)
 	router.HandleFunc("/heatindex", handlers.GetHeatIndex).Methods(http.MethodGet)
+
+	// router.HandleFunc("/actuate", BasicAuth(handlers.Actuate, user, pass, realm)).Methods(http.MethodGet)
+	// router.HandleFunc("/ping", BasicAuth(handlers.Ping, user, pass, realm)).Methods(http.MethodGet)
+	// router.HandleFunc("/doorstate", BasicAuth(handlers.GetState, user, pass, realm)).Methods(http.MethodGet)
+	// router.HandleFunc("/temperature", BasicAuth(handlers.GetTemperature, user, pass, realm)).Methods(http.MethodGet)
+	// router.HandleFunc("/humidity", BasicAuth(handlers.GetHumidity, user, pass, realm)).Methods(http.MethodGet)
+	// router.HandleFunc("/heatindex", BasicAuth(handlers.GetHeatIndex, user, pass, realm)).Methods(http.MethodGet)
 
 	c := cors.Default()
 	handler := c.Handler(router)
--- pkg/handlers/GarageHandlers.go
+++ pkg/handlers/GarageHandlers.go
@@ -1,11 +1,61 @@
 package handlers
 
 import (
+	"encoding/json"
+	"io"
+	"log"
 	"net/http"
+	"time"
 )
 
-func Actuate(w http.ResponseWriter, r *http.Request)        {}
-func GetState(w http.ResponseWriter, r *http.Request)       {}
-func GetTemperature(w http.ResponseWriter, r *http.Request) {}
-func GetHumidity(w http.ResponseWriter, r *http.Request)    {}
-func GetHeatIndex(w http.ResponseWriter, r *http.Request)   {}
+const microAddress string = "http://192.168.1.45:6262/"
+const defaultRequestTimeoutSeconds int = 5
+
+func Ping(w http.ResponseWriter, r *http.Request) {
+	sendGetRequest(w, "ping", defaultRequestTimeoutSeconds)
+}
+
+func Actuate(w http.ResponseWriter, r *http.Request) {
+	sendGetRequest(w, "actuate", defaultRequestTimeoutSeconds)
+}
+
+func GetState(w http.ResponseWriter, r *http.Request) {
+	sendGetRequest(w, "state", defaultRequestTimeoutSeconds)
+}
+
+func GetTemperature(w http.ResponseWriter, r *http.Request) {
+	sendGetRequest(w, "temperature", defaultRequestTimeoutSeconds)
+}
+
+func GetHumidity(w http.ResponseWriter, r *http.Request) {
+	sendGetRequest(w, "humidity", defaultRequestTimeoutSeconds)
+}
+
+func GetHeatIndex(w http.ResponseWriter, r *http.Request) {
+	sendGetRequest(w, "heatindex", defaultRequestTimeoutSeconds)
+}
+
+func sendGetRequest(w http.ResponseWriter, uri string, timeoutSeconds int) {
+	client := http.Client{
+		Timeout: time.Duration(timeoutSeconds) * time.Second,
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+
+	resp, err := client.Get(microAddress + uri)
+	if err != nil {
+		log.Println(err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Println(err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(body)
+}
